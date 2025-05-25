501da8998af9799f80344db9fc3f0232650f118e
Ahmad Sameh <ahmadsameh99@gmail.com>
2025-05-25 15:26:02 +0300

    Update on-start.sh

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -5,6 +5,6 @@ dockerd > /var/log/dockerd.log 2>&1 &
 sleep 2
 
 # pull solang image 
-docker pull ghcr.io/hyperledger/solang@sha256:e6f687910df5dd9d4f5285aed105ae0e6bcae912db43e8955ed4d8344d49785d 
+docker pull ghcr.io/hyperledger-solang/solang:latest
 
 cargo make run
