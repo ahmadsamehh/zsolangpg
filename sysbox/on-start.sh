3f6ae7241a332e6334737bdae3b528d13b495120
ahmadsamehh <ahmadsameh99@gmail.com>
2025-05-30 00:40:32 +0300

    updated on-start.sh

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+# #!/bin/bash
 
+# # dockerd start
+# dockerd > /var/log/dockerd.log 2>&1 &
+# sleep 3
+
+# # pull solang image
+# docker pull ghcr.io/hyperledger-solang/solang:latest
+
+# cargo make run
+
+
+#!/bin/bash
 # dockerd start
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 3
@@ -7,4 +18,7 @@ sleep 3
 # pull solang image
 docker pull ghcr.io/hyperledger-solang/solang:latest
 
-cargo make run
+# Execute the compiled backend directly instead of using cargo make
+# Ensure the path matches where it's copied in the Dockerfile
+/app/target/release/backend --frontend_folder /app/packages/app/dist --port 4444
+
